8376384e1dc776026e0335e4fa23f5ffdde1a355
Zheng Wang <zw494@cornell.edu>
2019-06-09 12:28:53 +0800

    Update os.md

---
+++ approximate-counter.c
@@ -0,0 +1,45 @@
+#include <pthread.h>
+#define NUMCPUS 10
+typedef struct __coutner_t
+{
+    int global;
+    pthread_mutex_t glock;
+    int local[NUMCPUS];
+    pthread_mutex_t llock[NUMCPUS];
+    int threshold;
+} counter_t;
+
+void init(counter_t *c, int threshold)
+{
+    c->threshold = threshold;
+    c->global = 0;
+    pthread_mutex_init(&c->glock, NULL);
+    for (int i = 0; i < NUMCPUS; i++)
+    {
+        c->local[i] = 0;
+        pthread_mutex_init(&c->llock[i], NULL);
+    }
+}
+
+void update(counter_t *c, int threadID, int amt)
+{
+    int cpu = threadID % NUMCPUS;
+    pthread_mutex_lock(&c->llock[cpu]);
+    c->local[cpu] += amt;
+    if (c->local[cpu] >= c->threshold)
+    {
+        pthread_mutex_lock(&c->glock);
+        c->global += c->local[cpu];
+        pthread_mutex_unlock(&c->glock);
+        c->local[cpu] = 0;
+    }
+    pthread_mutex_unlock(&c->llock[cpu]);
+}
+
+int get(counter_t *c)
+{
+    pthread_mutex_lock(&c->glock);
+    int val = c->global;
+    pthread_mutex_unlock(&c->glock);
+    return val; // only approximate!
+}
